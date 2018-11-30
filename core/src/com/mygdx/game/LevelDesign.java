816945ea3164d63c7bb8f8cbde5703c9cd3fd693
phub1604 <phub1604@HRH-IC0027330.wrdsb.ca>
2018-11-30 08:42:21 -0500

    New Classes

--- core/src/com/mygdx/game/LevelDesign.java
+++ core/src/com/mygdx/game/LevelDesign.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mygdx.game.desktop;
+package com.mygdx.game;
 
 /**
  *
  * @author prylz2189
  */
-public class Enemies {
+public class LevelDesign {
     
 }
--- core/src/com/mygdx/game/Weapon.java
+++ core/src/com/mygdx/game/Weapon.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mygdx.game.desktop;
+package com.mygdx.game;
 
 /**
  *
- * @author phub1604
+ * @author prylz2189
  */
-public class Player {
+public class Weapon {
     
 }
