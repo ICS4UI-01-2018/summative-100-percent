5079b143ad2ff07b0826924148eaaf4b64506195
Starm7481 <Starm7481@HRH-IC0027331.wrdsb.ca>
2018-12-20 08:10:55 -0500

    Merge origin/master
    Conflicts:
    	core/src/com/mygdx/game/Enemies.java
    	core/src/com/mygdx/game/Entity.java
    	core/src/com/mygdx/game/MyGdxGame.java
    	core/src/com/mygdx/game/Player.java

---
+++ core/src/com/mygdx/game/M1911.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game;
+
+/**
+ *
+ * @author prylz2189
+ */
+
+public class M1911 extends Weapon{
+
+    public M1911(float x, float y){
+        //Send variables to super class
+ //super(1, 11, 5, 30, x, y);
+}
+    
+}
---
+++ core/src/com/mygdx/game/M1911Bullet.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game;
+
+/**
+ *
+ * @author prylz2189
+ */
+public class M1911Bullet extends Bullet{
+    public M1911Bullet(float x, float y, int moveX, int moveY) {
+        //Send variables to super class
+        super(10, 5, x, y, 10, 10);
+}
+}
