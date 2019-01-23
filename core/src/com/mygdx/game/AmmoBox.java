f77f631065abb50aae2200d199ba90f5dd07b2ac
Brandon <Brandon@192.168.0.242>
2019-01-22 20:16:03 -0500

    - commenting

--- core/src/com/mygdx/game/AmmoBox.java
+++ core/src/com/mygdx/game/AmmoBox.java
@@ -13,18 +13,31 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  * @author phub1604
  */
 public class AmmoBox extends Item {
-    
+
     private Texture pic;
-    
-    public AmmoBox (float x, float y, int width, int height, float value) {
+
+    /**
+     * Initializes AmmoBox data.
+     *
+     * @param x the x position of the AmmoBox.
+     * @param y the y position of the AmmoBox.
+     * @param width the width of the AmmoBox.
+     * @param height the height of the AmmoBox.
+     * @param value the amount of bullets in the AmmoBox.
+     */
+    public AmmoBox(float x, float y, int width, int height, float value) {
         super(x, y, width, height, value);
         this.pic = new Texture("ammo.png");
     }
-    
+
+    /**
+     * Draws the AmmoBox.
+     *
+     * @param batch the SpriteBatch being used to draw.
+     */
     @Override
     public void draw(SpriteBatch batch) {
         batch.draw(this.pic, super.getX(), super.getY(), super.getWidth(), super.getHeight());
-    }    
-    
-    
+    }
+
 }
--- core/src/com/mygdx/game/Bullet.java
+++ core/src/com/mygdx/game/Bullet.java
@@ -20,36 +20,51 @@ public abstract class Bullet {
     private float y;
     private int damage;
     private float radius;
-
     private boolean isAlive;
     private float xDirection;
     private float yDirection;
-    
+    private boolean collided;
     private Rectangle bullet;
 
-    private boolean collided;
-    
+    /**
+     * Initializes Bullet data.
+     *
+     * @param speed the speed of the Bullet.
+     * @param x the x position of the Bullet.
+     * @param y the y position of the Bullet.
+     * @param damage the damage of the Bullet.
+     * @param radius the radius of the Bullet.
+     */
     public Bullet(float speed, float x, float y, int damage, float radius) {
-        //Set variables
         this.speed = speed;
         this.x = x;
         this.y = y;
         this.damage = damage;
         this.radius = radius;
 
+        // default variables
         this.isAlive = false;
         this.xDirection = 0;
         this.yDirection = 0;
-        
-        // create rectangle to deal with collision later
-        bullet = new Rectangle(x, y, radius, radius);
-        
         this.collided = false;
+
+        // Rectangle for collision
+        bullet = new Rectangle(x, y, radius, radius);
     }
 
-    // pass in Player and cursor coordinates
+    /**
+     * Moves the Bullet from the centre of the Player to where the cursor is
+     * clicked.
+     *
+     * @param player the Player where the angle is calculated from to the cursor
+     * click.
+     * @param cursorX the x position of where the cursor is clicked.
+     * @param cursorY the y position of where the cursor is clicked.
+     */
     public void move(Player player, float cursorX, float cursorY) {
+        // if Bullet hasn't been shot
         if (this.isAlive == false) {
+            // calculate angle and directions
             float angle = (float) (Math.atan2((cursorY - (player.getY() + (player.getHeight() / 2))), (cursorX - (player.getX() + (player.getWidth() / 2)))));
             this.xDirection = (float) (Math.cos(angle));
             this.yDirection = (float) (Math.sin(angle));
@@ -62,49 +77,82 @@ public abstract class Bullet {
         // move X and Y coordinates of bullet
         this.x = this.x + (this.xDirection * this.speed);
         this.y = this.y + (this.yDirection * this.speed);
-
     }
 
+    /**
+     * Returns the x position of the Bullet.
+     *
+     * @return the float representing the Bullet's x position.
+     */
     public float getX() {
         return this.x;
     }
 
+    /**
+     * Returns the y position of the Bullet.
+     *
+     * @return the float representing the Bullet's y position.
+     */
     public float getY() {
         return this.y;
     }
 
-    //Get damage
+    /**
+     * Returns the damage of the Bullet.
+     *
+     * @return the integer representing the Bullet's damage.
+     */
     public int getDamage() {
         return this.damage;
     }
 
-    //Get speed
+    /**
+     * Returns the speed of the Bullet.
+     *
+     * @return the float representing the Bullet's speed.
+     */
     public float getSpeed() {
         return this.speed;
     }
 
-    // get radius
+    /**
+     * Returns the radius of the Bullet.
+     *
+     * @return the float representing the Bullet's radius.
+     */
     public float getRadius() {
         return this.radius;
     }
 
-    //Is the bullet alive
+    /**
+     * Returns the alive state of the Bullet.
+     *
+     * @return returns true if Bullet has been shot, false if it hasn't.
+     */
     public boolean getIsAlive() {
         return this.isAlive;
     }
 
+    /**
+     * Set the Bullet to be alive (has been shot).
+     */
     public void setAlive() {
         this.isAlive = true;
     }
 
-    //Draw bullet
+    /**
+     * Draws the Bullet.
+     *
+     * @param shapeBatch the ShapeRenderer being used to draw.
+     */
     public void drawBullet(ShapeRenderer shapeBatch) {
         shapeBatch.circle(this.x, this.y, this.radius);
     }
 
     /**
-     * Returns the rectangle of the bullet.
-     * @return the rectangle representing the bullet.
+     * Returns the rectangle of the Bullet.
+     *
+     * @return the rectangle representing the Bullet.
      */
     public Rectangle getShape() {
         bullet.x = this.x;
@@ -112,14 +160,21 @@ public abstract class Bullet {
         return bullet;
     }
 
+    /**
+     * Returns the collision state of the Bullet.
+     *
+     * @return returns true if the Bullet has collided, false otherwise.
+     */
     public boolean getCollided() {
         return this.collided;
     }
-    
+
+    /**
+     * Sets the Bullet in the collided state.
+     */
     public void setCollided() {
         this.collided = true;
         // set damage to 0 so that you cannot damage through walls
         this.damage = 0;
     }
-    
 }
